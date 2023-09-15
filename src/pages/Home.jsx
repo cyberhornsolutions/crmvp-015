25b2ca9acef1c1a68fa02a30be80f3f77714e935
Vadym Chumasov <u2107680007911543774e7r@Vadyms-MacBook-Air.local>
2023-09-16 00:09:50 +0300

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <button>xdemi-011</button>
+      <a href="./Leads"> lin k</a>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Leads.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Leads() {
+  return (
+    <div><button>arbuzatton</button></div>
+  )
+}
+
+export default Leads
