f94113a50c405c3fc4d653a9a85cbc69dd6d94b2
u2107680007911543774e7r <u2107680007911543774e7r@gmail.com>
2023-10-09 18:05:36 +0300

    ready to convert

--- src/tabs-en.js
+++ src/tabs-en.js
@@ -42,6 +42,10 @@ const tabIDs = [
 ];
 
 const tabtitle = document.getElementById('tab-title')
+const leadnameEl = document.getElementById('lead-name')
+const profilename = document.getElementById('profile-name')
+
+const leadTransactionsName = document.getElementById('lead-transactions-name')
 
 function showMain() {
   menuIDs.forEach(id => {
@@ -60,7 +64,7 @@ function showMain() {
   
   tabmain.style.display = 'flex';
   menumain.classList.add('active')
-  tabtitle.textContent = 'Mainboard'
+  tabtitle.textContent = 'Player'
 }
 
 // Function to hide the leads table and show the dashboard
@@ -86,6 +90,9 @@ function showDashboard() {
 
 // Function to hide the dashboard and show the leads table
 function showLeads() {
+  const leadsDiv = document.getElementById('leads-div')
+  const leadTransactionsDiv = document.getElementById('lead-transactions')
+
   menuIDs.forEach(id => {
     const element = document.getElementById(id);
     if (element.classList.contains('active')) {
@@ -102,7 +109,9 @@ function showLeads() {
 
   tableads.style.display = 'flex'
   menuleads.classList.add('active')
-  tabtitle.textContent = 'Leads'
+  leadsDiv.classList.remove('hidden')
+  leadTransactionsDiv.classList.remove('hidden')
+  tabtitle.textContent = 'Players'
 }
 
 function showTransactions() {
@@ -126,6 +135,9 @@ function showTransactions() {
 }
 
 function showUsers() {
+  const usersDiv = document.getElementById('users-div')
+  const newUserForm = document.getElementById('newUser-form')
+
   menuIDs.forEach(id => {
     const element = document.getElementById(id);
     if (element.classList.contains('active')) {
@@ -139,10 +151,11 @@ function showUsers() {
       element.style.display = 'none'
     }
   });
-
+  usersDiv.classList.remove('hidden')
+  newUserForm.classList.remove('hidden')
   tabusers.style.display = 'flex';
   menuusers.classList.add('active')
-  tabtitle.textContent = 'Users'
+  tabtitle.textContent = 'Managers'
 }
 
 function showCalendar() {
@@ -164,6 +177,42 @@ function showCalendar() {
   menucalendar.classList.add('active')
   tabtitle.textContent = 'Calendar'
 }
+
+const leadsTableRows = document.querySelectorAll("#leadsTable tbody tr");
+const leadCardButton = document.getElementById('lead-card-button')
+const leadTransactionsBody = document.querySelector('#lead-transactions-bottom-table tbody')
+
+// Loop through each row and add a click event listener
+leadsTableRows.forEach((row) => {
+  row.addEventListener("click", function () {
+    // Get the name from the clicked row
+    const leadName = row.querySelector("td:nth-child(3)").textContent;
+    leadTransactionsName.textContent = leadName;
+    leadTransactionsBody.classList.remove('hidden');
+  });
+});
+
+leadCardButton.addEventListener('click', function() {
+  menuIDs.forEach(id => {
+    const element = document.getElementById(id);
+    if (element.classList.contains('active')) {
+      element.classList.remove('active');
+    }
+  });
+  
+  tabIDs.forEach(id => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.style.display = 'none'
+    }
+  });
+    
+  tabmain.style.display = 'flex';
+  menumain.classList.add('active')
+  tabtitle.textContent = 'Player card';
+  leadnameEl.textContent = leadTransactionsName.textContent;
+  profilename.placeholder = leadTransactionsName.textContent;
+})
 
 // Add click event listeners to the menu items
 menumain.addEventListener('click', showMain)
@@ -194,3 +243,19 @@ function myFunction() {
     }
   }
 }
+// Get a reference to the button element
+var logoutButton = document.getElementById("logout-button");
+
+// Add a click event listener to the button
+logoutButton.addEventListener("click", function() {
+    // Redirect the user to the login.html page
+    window.location.href = "login-crm.html";
+});
+
+var platformLink = document.getElementById('menu-platform');
+
+platformLink.addEventListener('click', function() {
+  const urlToOpen = 'login.html';
+            // Open the URL in a new tab
+            window.open(urlToOpen, '_blank');
+})
