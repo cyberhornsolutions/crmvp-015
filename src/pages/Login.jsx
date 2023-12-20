95cca53f4c67bf55e69075b706b70ebd6ff32a5e
Mubasher Rasheed <bitf12m001@gmail.com>
2023-12-20 16:42:10 +0500

    login username password states added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,52 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../logo.png";
 import { useNavigate } from "react-router-dom";
+import { Form } from "react-bootstrap";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function Login() {
   const navigate = useNavigate();
 
+  const [role, setRole] = useState("");
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!role) {
+      toast.error("Please select a role");
+    } else {
+      navigate("/home");
+    }
+  };
+
+  const handleChange = (e) =>
+    setUser((p) => ({ ...p, [e.target.name]: e.target.value }));
+
   return (
     <div
-      className="login_form"
-      // action="main-crm.html"
+      className="d-flex align-items-center"
+      style={{
+        height: "100vh",
+      }}
     >
-      <div id="logo_wrapper" className="logo_wrapper">
-        <img className="logo" src={logo} alt="logo" />
-      </div>
-      <div id="title">
-        <h2>CRM</h2>
-      </div>
-      <div className="admin_role">
-        <input type="radio" value="Admin" name="role" /> <span>Admin</span>
-      </div>
-      <div className="fields">
-        <input
-          className="email_input  text-left"
-          type="email"
-          placeholder="Email"
-          name="email"
-          required
+      <ToastContainer />
+      <Form
+        onSubmit={handleSubmit}
+        className="d-flex flex-column align-items-center mx-auto login_form"
+      >
+        <div id="logo_wrapper" className="logo_wrapper">
+          <img className="logo" src={logo} alt="logo" />
+        </div>
+        <div id="title">
+          <h2>CRM</h2>
+        </div>
+        <Form.Check
+          type="radio"
+          label="Admin"
+          id="admin_role"
+          className="text-light"
+          checked={role === "Admin"}
+          onClick={() => setRole("Admin")}
         />
+        <div className="w-75">
+          <input
+            className="email_input text-left"
+            type="text"
+            placeholder="Username"
+            value={user.username}
+            name="username"
+            required
+            onChange={handleChange}
+          />
 
-        <input
-          className="psw_input  text-left"
-          type="password"
-          placeholder="Password"
-          name="psw"
-          required
-        />
-      </div>
-      <div className="other_roles">
-        <div className="other_role">
-          <input type="radio" value="Sale" name="role" /> <span>Sale</span>
+          <input
+            className="psw_input text-left"
+            type="password"
+            placeholder="Password"
+            value={user.password}
+            name="password"
+            required
+            onChange={handleChange}
+          />
         </div>
-        <div className="other_role">
-          <input type="radio" value="Reten" name="role" /> <span>Reten</span>
+        <div className="row mt-4">
+          <div className="col">
+            <Form.Check
+              type="radio"
+              label="Sale"
+              id="sale_role"
+              className="text-light"
+              checked={role === "Sale"}
+              onClick={() => setRole("Sale")}
+            />
+          </div>
+          <div className="col">
+            <Form.Check
+              type="radio"
+              label="Reten"
+              id="Reten_role"
+              className="text-light"
+              checked={role === "Reten"}
+              onClick={() => setRole("Reten")}
+            />
+          </div>
         </div>
-      </div>
-      <button className="button" onClick={() => navigate("/home")}>
-        Enter
-      </button>
+        <button className="button" type="submit">
+          Enter
+        </button>
+      </Form>
     </div>
   );
 }
